8aba87e55c69d842fc7626ba2dc53bd3c11e696d
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-04 15:10:59 +0100

    Adds steps (comments) to follow

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-console.log('VUE OK', Vue);
-console.log('AXIO OK', axios);
+// console.log('VUE OK', Vue);
+// console.log('AXIO OK', axios);
+
+// https://flynn.boolean.careers/exercises/api/random/mail
+// Usando Vue.js, generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
 Vue.config.devtools = true;
 
 const root = new Vue({
